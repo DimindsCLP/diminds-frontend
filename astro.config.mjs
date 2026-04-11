f3c4b47f21240a1498df2425e8928411c3704432
cgutigo <150248693+cgutigo@users.noreply.github.com>
2026-04-11 12:53:00 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,3 @@
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel';
 
-export default defineConfig({
-  adapter: vercel(),
-  output: 'server'
-});
+export default defineConfig({});
